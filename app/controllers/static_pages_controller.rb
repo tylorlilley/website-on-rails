14c0466d3b71a71fef0cc19e4aa37d5a1c23ec77
Tylor Lilley <tlilley@covermymeds.com>
2019-10-23 16:07:20 -0400

    Remove encryption attempts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,16 +14,4 @@ class StaticPagesController < ApplicationController
 
   def follow
   end
-
-  def encrypt1
-    render html: "tBc2N67HZ66xVhKf4LBabXkwHiCX3XR78YXsM2grPVc.apAwhPX7rZpFf6lZJkT2c4AmgC7lyAWv3GRvqgb3luM"
-  end
-
-  def encrypt2
-    render html: "E9i4kw3XP43Y2CQKiP_bvKFO_xjHdBKLb9dMz2Tk9gA.apAwhPX7rZpFf6lZJkT2c4AmgC7lyAWv3GRvqgb3luM"
-  end
-
-  def encrypt3
-    render html: "ZEHgr2pKwlhRtFqILnwlPGrHgs8jTOayLbEOkyztF0I.apAwhPX7rZpFf6lZJkT2c4AmgC7lyAWv3GRvqgb3luM"
-  end
 end
