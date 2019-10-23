88bbbf4a863b1ee257c694e84e7daa403fc1f050
Tylor Lilley <tlilley@covermymeds.com>
2019-10-23 15:53:49 -0400

    Fix encrypt controller actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,10 +16,10 @@ class StaticPagesController < ApplicationController
   end
 
   def encrypt1
-    render text: "tBc2N67HZ66xVhKf4LBabXkwHiCX3XR78YXsM2grPVc.apAwhPX7rZpFf6lZJkT2c4AmgC7lyAWv3GRvqgb3luM"
+    render plain: "tBc2N67HZ66xVhKf4LBabXkwHiCX3XR78YXsM2grPVc.apAwhPX7rZpFf6lZJkT2c4AmgC7lyAWv3GRvqgb3luM"
   end
 
   def encrypt2
-    render text: "E9i4kw3XP43Y2CQKiP_bvKFO_xjHdBKLb9dMz2Tk9gA.apAwhPX7rZpFf6lZJkT2c4AmgC7lyAWv3GRvqgb3luM"
+    render plain: "E9i4kw3XP43Y2CQKiP_bvKFO_xjHdBKLb9dMz2Tk9gA.apAwhPX7rZpFf6lZJkT2c4AmgC7lyAWv3GRvqgb3luM"
   end
 end
